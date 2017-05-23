1b9d69bd7590ad29766fb0b993ca75748263634e
fwuen <git@fwuen.de>
2017-05-23 16:17:10 +0200

    Controller erstellt

---
+++ app/Http/Controllers/DocentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Docent;
+use Illuminate\Http\Request;
+
+class DocentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Docent  $docent
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Docent $docent)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Docent  $docent
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Docent $docent)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Docent  $docent
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Docent $docent)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Docent  $docent
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Docent $docent)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/MeetingSeriesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\MeetingSeries;
+use Illuminate\Http\Request;
+
+class MeetingSeriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\MeetingSeries  $meetingSeries
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MeetingSeries $meetingSeries)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\MeetingSeries  $meetingSeries
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MeetingSeries $meetingSeries)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\MeetingSeries  $meetingSeries
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MeetingSeries $meetingSeries)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\MeetingSeries  $meetingSeries
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MeetingSeries $meetingSeries)
+    {
+        //
+    }
+}
