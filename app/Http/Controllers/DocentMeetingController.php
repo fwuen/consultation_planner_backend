e6ae2e82ca24e909e6489407a57586f76460d369
fwuen <git@fwuen.de>
2017-06-27 20:06:14 +0200

    Meeting ohne validate

--- app/Http/Controllers/DocentMeetingController.php
+++ app/Http/Controllers/DocentMeetingController.php
@@ -252,6 +252,7 @@ class DocentMeetingController extends Controller
 
     private function doBasicRequestValidation(Request $request)
     {
+        /*
         $this->validate($request, [
             'start' => 'required|date',
             'end' => 'required|date|after:start',
@@ -261,7 +262,7 @@ class DocentMeetingController extends Controller
             'description' => 'required|max:500',
             'room' => 'required|max:10',
             'last_enrollment' => 'required|date|before:start'
-        ]);
+        ]);*/
     }
 
     private function setMeetingProperties(Meeting $meeting, Request $request)
