7a19d2db389a5e511a5da56b6bd28bbb6bf5c689
fwuen <git@fwuen.de>
2017-07-04 15:32:22 +0200

    Prüfen, ob Student schon an Meeting teilnimmt

--- app/Http/Controllers/StudentParticipationController.php
+++ app/Http/Controllers/StudentParticipationController.php
@@ -54,6 +54,10 @@ class StudentParticipationController extends Controller
         $meeting = Meeting::findOrFail($participation->meeting_id);
 
         if ($meeting->slots == 1) {
+            $participations = Participation::where('student_id', '=', $id)->where('meeting_id', '=', $meeting->id)->get();
+            if($participations.count() > 0) {
+                return;
+            }
             $meeting->participants_count = $meeting->participants_count + 1;
             if ($meeting->participants_count <= $meeting->max_participants) {
                 $participation->save();
